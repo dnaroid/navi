c702945ca1c0a46f3ce90dc2f254e676798edc71
vt <vt@anadeainc.com>
2024-08-31 15:04:30 +0200

    working WS cam

--- lib/Mirror/Mirror.cpp
+++ lib/Mirror/Mirror.cpp
@@ -6,9 +6,10 @@
 
 #include "WebSocketsServer.h"
 
-WebSocketsServer webSocket(81);
+static WebSocketsServer webSocket(81);
 extern TFT_eSPI* tft_inst;
-
+static bool camEnabled = false;
+static int camNum = -1;
 
 static bool tft_output(int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t* bitmap) {
   tft_inst->pushImage(x, y, w, h, bitmap);
@@ -18,11 +19,13 @@ static bool tft_output(int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t* b
 static void webSocketEvent(uint8_t num, WStype_t type, uint8_t* payload, size_t length) {
   switch (type) {
   case WStype_DISCONNECTED:
-    Serial.printf("[%u] Disconnected\n", num);
+    LOGF("[%u] Disconnected\n", num);
+    camNum = -1;
     break;
   case WStype_CONNECTED:
-    Serial.printf("[%u] Connected\n", num);
-    webSocket.sendTXT(num, "image");
+    LOGF("[%u] Connected\n", num);
+    camNum = num;
+    if (camEnabled) webSocket.sendTXT(num, "start");
     break;
   case WStype_BIN:
     TJpgDec.drawJpg(0, 0, payload, length);
@@ -37,16 +40,19 @@ void Mirror_init() {
   TJpgDec.setSwapBytes(true);
   TJpgDec.setCallback(tft_output);
   WiFi.softAP(WIFI_SSID, WIFI_PASSWORD);
+  LOG("WS server IP:", WiFi.softAPIP());
   webSocket.begin();
   webSocket.onEvent(webSocketEvent);
 }
 
 void Mirror_start() {
-  webSocket.broadcastTXT("camera.start");
+  camEnabled = true;
+  if (camNum != -1) webSocket.sendTXT(camNum, "start");
 }
 
 void Mirror_stop() {
-  webSocket.broadcastTXT("camera.stop");
+  camEnabled = false;
+  if (camNum != -1) webSocket.sendTXT(camNum, "stop");
 }
 
 void Mirror_loop() {
